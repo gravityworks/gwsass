7261770195abd7a5be67cbb5729b5d9b4bc34f4c
Heather Buchel <hbuchel@gmail.com>
2014-04-16 22:16:34 -0400

    Update GruntFile.js

--- GruntFile.js
+++ GruntFile.js
@@ -1,28 +1,62 @@
 module.exports = function(grunt) {
- 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    compass: {
-      options: {
-        config: 'config.rb',
-        force: true
-      }
-    },
-    watch: {
-      css: {
-        files: ['sass/{,**/}*.scss'],
-        tasks: ['compass']
-      }
-    }
-    
-  });
- 
-  // Load plugins
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
- 
-  // Default task(s).
-  grunt.registerTask('default', ['watch']);
- 
-};
+
+ // load all grunt tasks
+ require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+ grunt.initConfig({
+
+   compass: {
+     dev: {
+       options: {
+         config: 'config.rb',
+         force: true
+       }
+     }
+   },
+   grunticon: {
+     myIcons: {
+       files: [{
+           expand: true,
+           cwd: 'images/svg-min',
+           src: ['*.svg', '*.png'],
+           dest: "images/svg-output"
+       }],
+       options: {
+         datasvgcss: '../../sass/base/_svg-data.scss'
+       }
+     }
+   },
+   svgmin: {                       
+     options: {             
+         plugins: [{
+             removeViewBox: false
+         }, {
+             removeUselessStrokeAndFill: false
+         }]
+     },
+     dist: {                     
+         files: [{              
+             expand: true,       
+             cwd: 'images/svg-source',     
+             src: ['**/*.svg'],  
+             dest: 'images/svg-min'
+         }]
+     }
+   },
+   watch: {
+     sass: {
+       files: ['sass/**/*.scss'],
+       tasks: ['compass:dev']
+     },
+     svg: {
+       files: ['images/svg-source/*.svg'],
+       tasks: ['svgmin','grunticon']
+       
+     }
+   }
+
+ });
+
+  grunt.registerTask('default', 'watch');
+
+}
