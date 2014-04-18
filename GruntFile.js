78d97a5a9d2f04da2780a1ff87cba4667396232c
Heather Buchel <hbuchel@gmail.com>
2014-04-18 11:34:09 -0400

    Updated GruntFile, got rid of grunticon

--- GruntFile.js
+++ GruntFile.js
@@ -1,61 +1,67 @@
 module.exports = function(grunt) {
 
- // load all grunt tasks
- require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
- grunt.initConfig({
+  grunt.initConfig({
 
-   compass: {
-     dev: {
-       options: {
-         config: 'config.rb',
-         force: true
-       }
-     }
-   },
-   grunticon: {
-     myIcons: {
-       files: [{
-           expand: true,
-           cwd: 'images/svg-min',
-           src: ['*.svg', '*.png'],
-           dest: "images/svg-output"
-       }],
-       options: {
-         datasvgcss: '../../sass/base/_svg-data.scss'
-       }
-     }
-   },
-   svgmin: {                       
-     options: {             
-         plugins: [{
-             removeViewBox: false
-         }, {
-             removeUselessStrokeAndFill: false
-         }]
-     },
-     dist: {                     
-         files: [{              
-             expand: true,       
-             cwd: 'images/svg-source',     
-             src: ['**/*.svg'],  
-             dest: 'images/svg-min'
-         }]
-     }
-   },
-   watch: {
-     sass: {
-       files: ['sass/**/*.scss'],
-       tasks: ['compass:dev']
-     },
-     svg: {
-       files: ['images/svg-source/*.svg'],
-       tasks: ['svgmin','grunticon']
-       
-     }
-   }
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb',
+          force: true
+        }
+      }
+    },
+    "svg-sprites": {
+        options: { /* These are Global Options */
+            spriteElementPath: "images/svg-icons-source",
+            spritePath: "images/svg-min/sprites",
+            cssPath: "images/svg-source",
+            cssPrefix: "_icon"
+        },
+        misc: { /* These are options specific to the icons in our misc folder. */
+          options: {
+            sizes: {
+              std: 18
+            },
+            refSize: 17
+          }
+        }
+    },
+    svgmin: {                       
+      options: {             
+          plugins: [{
+              removeViewBox: false
+          }, {
+              removeUselessStrokeAndFill: false
+          }]
+      },
+      dist: {                     
+          files: [{              
+              expand: true,       
+              cwd: 'images/svg-source',     
+              src: ['**/*.svg'],  
+              dest: 'images/svg-min'
+          }]
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['compass:dev']
+      },
+      svg: {
+        files: ['images/svg-source/*.svg'],
+        tasks: ['svgmin']
+      },
+      svgicons: {
+        files: ['images/svg-icons-source/**/*.svg'],
+        tasks: ['svg-sprites']
+      }
+    }
 
- });
+  });
 
   grunt.registerTask('default', 'watch');
 
